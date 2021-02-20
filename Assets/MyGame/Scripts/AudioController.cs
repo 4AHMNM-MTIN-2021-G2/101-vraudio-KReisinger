2b9bb751202f496853ca87241345235d4a01d276
Kilian Reisinger <kilian.reisinger02@htl-salzburg.ac.at>
2021-02-20 17:41:53 +0100

    audio file implementation

--- Assets/MyGame/Scripts/AudioController.cs
+++ Assets/MyGame/Scripts/AudioController.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public AudioSource softWind;
+    public AudioSource heavyRainStorm;
+    public AudioSource rain;
+    public AudioSource softRainStorm;
+
     void Start()
     {
-        
+        softRainStorm.Play();
+       
     }
 
     // Update is called once per frame
